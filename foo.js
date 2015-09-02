5f8577f5149f8fe5da26af4c0f368c7c9d8b16ab
Rob Drimmie <rob@drimmie.net>
2015-09-02 13:23:10 -0400

    how bad can I make the score?

--- foo.js
+++ foo.js
@@ -1,3 +1,230 @@
+
+
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
+lksdfvar foo = function() {
+asdflkjasdf	console.log( 'ooooo' );
+}asdflkajsdfalkj
+
+foo();asdfl;kjasfdj
 lksdfvar foo = function() {
 asdflkjasdf	console.log( 'ooooo' );
 }asdflkajsdfalkj
